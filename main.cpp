53f1dae69dd857a57b2bfc0d5e74fb9f806f79aa
ReinhardProbst <reinhardprobst42@gmail.com>
2017-02-16 15:14:57 +0100

    Made invoke simpler and added task group and queue sample

--- main.cpp
+++ main.cpp
@@ -10,23 +10,65 @@
 
 #include <tbb/tbb.h>
 
+tbb::concurrent_bounded_queue<int> queue;
+
 void f1()
 {
+#ifdef __linux__
+    pid_t tid = syscall(SYS_gettid);
+    std::cout << "Tid of job f1: " << tid<< std::endl;
+#endif
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
     std::cout << "Job f1 done ..." << std::endl;
 };
 
 void f2()
 {
-    std::cout << "Job gf2 done ..." << std::endl;
+#ifdef __linux__
+    pid_t tid = syscall(SYS_gettid);
+    std::cout << "Tid of job f2: " << tid<< std::endl;
+#endif
+
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::cout << "Job f2 done ..." << std::endl;
 };
 
-void foo(const tbb::blocked_range<size_t>& r)
+void f3(const tbb::blocked_range<size_t>& r)
 {
+#ifdef __linux__
+    pid_t tid = syscall(SYS_gettid);
+    std::cout << "Tid of job f3: " << tid<< std::endl;
+#endif
+
     std::this_thread::sleep_for(std::chrono::seconds(5));
     std::cout << r.begin() << "/"<< r.end() << std::endl;
     std::cout << "Job done ..." << std::endl;
 };
 
+void f4()
+{
+#ifdef __linux__
+    pid_t tid = syscall(SYS_gettid);
+    std::cout << "Tid of job f1: " << tid<< std::endl;
+#endif
+
+    int i;
+    queue.pop(i);
+    std::cout << "Job f4 done, got: " << i << std::endl;
+};
+
+void f5()
+{
+#ifdef __linux__
+    pid_t tid = syscall(SYS_gettid);
+    std::cout << "Tid of job f2: " << tid<< std::endl;
+#endif
+
+    int i;
+    queue.pop(i);
+    std::cout << "Job f5 done, got: " << i << std::endl;
+};
 struct Observer : tbb::task_scheduler_observer
 {
     int _rt_prio;
@@ -58,17 +100,18 @@ struct Observer : tbb::task_scheduler_observer
     void on_scheduler_exit(bool) { std::cout << "Off:" << pthread_self() << std::endl; }
 };
 
-constexpr int loop_cnt = 3;
+constexpr int loop_cnt = 1;
 constexpr int max_threads = 4;
 
 int main(void)
 {
     Observer observer;
     tbb::task_scheduler_init init(max_threads);
+    tbb::task_group tg;
 
     std::cout << "TBB threads, max available: " << init.default_num_threads() << std::endl;
 
-    std::function<void(const tbb::blocked_range<size_t>& r)> f = foo;
+    std::function<void(const tbb::blocked_range<size_t>& r)> f = f3;
 
     {
         boost::timer::auto_cpu_timer act;
@@ -79,9 +122,21 @@ int main(void)
 
             std::cout << "All _for_ jobs done ..." << std::endl;
 
-            tbb::parallel_invoke([](){f1();}, [](){f2();});
+            tbb::parallel_invoke(f1, f2);
 
             std::cout << "All _invoke_ jobs done ..." << std::endl;
+
+            tg.run([](){f4();});
+            tg.run([](){f5();});
+
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+
+            queue.push(-1);
+            queue.push(-2);
+
+            tg.wait();
+
+            std::cout << "All _taskgroup_ jobs done ..." << std::endl;
         }
     }
 
